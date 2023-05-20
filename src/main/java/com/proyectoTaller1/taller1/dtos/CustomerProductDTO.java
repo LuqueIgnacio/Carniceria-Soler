748fe2997afe7f3f58e694556784047ffedc5333
LuqueIgnacio <111322751+LuqueIgnacio@users.noreply.github.com>
2023-05-20 12:53:25 -0300

    List products in admin panel (#2)
    * Added functionality to list all products for sale.
    
    * Added functionality to list all products in the admin panel

--- src/main/java/com/proyectoTaller1/taller1/dtos/CustomerProductDTO.java
+++ src/main/java/com/proyectoTaller1/taller1/dtos/CustomerProductDTO.java
@@ -13,7 +13,7 @@ public class CustomerProductDTO {
     private Long id;
     private String name;
     private String image;
-    private BigDecimal price = new BigDecimal(0.0);
+    private BigDecimal price;
     private CustomerCategoryDTO category;
     private CustomerBrandDTO brand;
 }
---
+++ src/main/java/com/proyectoTaller1/taller1/mappers/AdminProductMapper.java
@@ -0,0 +1,17 @@
+package com.proyectoTaller1.taller1.mappers;
+
+import com.proyectoTaller1.taller1.dtos.AdminProductDTO;
+import com.proyectoTaller1.taller1.models.Brand;
+import com.proyectoTaller1.taller1.models.Category;
+import com.proyectoTaller1.taller1.models.Product;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(uses = {CustomerCategoryMapper.class, CustomerBrandMapper.class},componentModel = "spring", builder = @Builder(disableBuilder = true))
+public interface AdminProductMapper {
+    @Mapping(target = "id", source = "product.id")
+    @Mapping(target = "name", source = "product.name")
+    @Mapping(target = "deletedAt", source = "product.deletedAt")
+    AdminProductDTO toDTO(Product product, Category category, Brand brand);
+}
