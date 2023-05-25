01a86fae547a1d2f637fc04acaec4bb143aadc82
LuqueIgnacio <111322751+LuqueIgnacio@users.noreply.github.com>
2023-05-25 14:13:38 -0300

    Added functionality to list one and more Users (#7)

--- src/main/java/com/proyectoTaller1/taller1/controllers/UsersController.java
+++ src/main/java/com/proyectoTaller1/taller1/controllers/UsersController.java
@@ -1,22 +1,41 @@
 package com.proyectoTaller1.taller1.controllers;
 
 import com.proyectoTaller1.taller1.dtos.AdminUserDTO;
+import com.proyectoTaller1.taller1.mappers.AdminUserMapper;
 import com.proyectoTaller1.taller1.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 @AllArgsConstructor
 public class UsersController {
     private final UserService userService;
+
+    @GetMapping("users")
+    public ResponseEntity<List<AdminUserDTO>> getAllAdminUser(){
+        return new ResponseEntity<>(userService.getAllAdminUserDTO(), HttpStatus.OK);
+    }
+
+    @GetMapping("users/{id}")
+    public ResponseEntity<AdminUserDTO> getAdminUser(@PathVariable Long id){
+        try{
+            return new ResponseEntity<>(userService.getAdminUserDTO(id), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
     @PostMapping("users")
     public ResponseEntity<Void> create(@Valid AdminUserDTO userDTO, Errors errors){
         if(errors.hasErrors()){
--- src/main/java/com/proyectoTaller1/taller1/repositories/UserRepository.java
+++ src/main/java/com/proyectoTaller1/taller1/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package com.proyectoTaller1.taller1.repositories;
 
 import com.proyectoTaller1.taller1.models.User;
+import org.mapstruct.control.MappingControl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByDeletedAtFalse();
 }
